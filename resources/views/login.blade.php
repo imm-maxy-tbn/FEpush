a7b8de56e496cc43ec1a8db9ac5f0a456b93bf06
ghaydarafa <ghayda.rafa@gmail.com>
2024-06-02 18:32:27 +0700

    merge

---
+++ resources/views/login.blade.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Homepage</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" />
+    <link rel="stylesheet" href="{{ asset('css/Login.css') }}">
+    <link
+      rel="stylesheet"
+      href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css"
+      integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A=="
+      crossorigin="anonymous"
+      referrerpolicy="no-referrer"
+    />
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet" />
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container">
+        <a class="navbar-brand" href="#">IMM Impact Mate</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+            <li class="nav-item active">
+            <a class="nav-link" href="welcome">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Bootcamp</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Community</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Profile</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="login-container">
+    <div class="login-form">
+      <img src="images/imm.png" alt="Brand Logo" class="brand-logo">
+      <h2>Masuk ke akun anda</h2>
+      <div class="input-group">
+        <label for="email">Email</label>
+        <span class="input-icon"><i class="fas fa-envelope"></i></span>
+        <input type="email" id="email" placeholder="abcdef@mail.com" />
+      </div>
+      <div class="input-group">
+        <label for="password">Password</label>
+        <span class="input-icon"><i class="fas fa-lock"></i></span>
+        <input type="password" id="password" placeholder="abcdef@mail.com" />
+      </div>
+      <button class="btn-login" id="masukBtn">Masuk</button>
+      <div class="register-link">Belum punya akun? <a href="register">Daftar</a></div>
+    </div>
+  </div>
+
+    <div class="image-container">
+      <img src="images/6.png" alt="Your Image" />
+    </div>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="{{ asset('js/login.js') }}"></script>
+  </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -54,6 +54,10 @@ Route::get('/creatp', function () {
     return view('creatp');
 });
 
+Route::get('/creatp1', function () {
+    return view('creatp1');
+});
+
 use App\Http\Controllers\HomeController;
 Route::get('/home', [HomeController::class, 'index'])->name('home');
 
@@ -64,61 +68,3 @@ use App\Http\Controllers\OTPController;
 
 Route::post('/send-otp', [OTPController::class, 'sendOTP']);
 Route::post('/verify-otp', [OTPController::class, 'verifyOTP']);
-
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/welcome', function () {
-    return view('welcome');
-})->name('home'); /* ini befungsi untuk memindahkan dari login ke home */
-
-Route::get('/login', function () {
-    return view('login');
-}); /* ini befungsi untuk memindahkan ke halaman login  */
-
-Route::get('/register', function () {
-    return view('register');
-});
-
-Route::get('/imm', function () {
-    return view('imm');
-});
-
-Route::get('/imm2', function () {
-    return view('imm2');
-});
-
-Route::get('/imm3', function () {
-    return view('imm3');
-});
-
-Route::get('/imm4', function () {
-    return view('imm4');
-});
-
-Route::get('/homepage', function () {
-    return view('homepage');
-});
-
-Route::get('/myproject', function () {
-    return view('myproject');
-});
-
-Route::get('/survey', function () {
-    return view('survey');
-});
-
-Route::get('/creatp', function () {
-    return view('creatp');
-});
-
-Route::get('/creatp1', function () {
-    return view('creatp1');
-}); 
