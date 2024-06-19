be8bd63fd88b4a5914e0e167eb5211423f59aa15
ghaydarafa <ghayda.rafa@gmail.com>
2024-06-19 22:41:08 +0700

    login regis update for survey purpose

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
 
 class LoginController extends Controller
 {
@@ -37,4 +40,59 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+{
+    $credentials = $this->credentials($request);
+    $user = \App\Models\User::where('email', $credentials['email'])->first();
+
+    // Check for user existence and complete profile ONLY for 'USER' role
+    if ($user && $user->role === 'USER') {
+        if (is_null($user->nik) || is_null($user->negara) || is_null($user->provinsi)) {
+            Log::info('User with incomplete profile tried to log in.', ['user_id' => $user->id]);
+            return false;
+        }
+    }
+
+    // Attempt login for all users, regardless of role
+    $attempt = Auth::attempt($credentials, $request->filled('remember'));
+
+    if ($attempt) {
+        Log::info('Login attempt successful for user: ' . $user->email);
+    } else {
+        Log::info('Login attempt failed for user: ' . $credentials['email']);
+    }
+
+    return $attempt;
+}
+
+
+
+
+protected function sendFailedLoginResponse(Request $request)
+{
+    $user = \App\Models\User::where('email', $request->email)->first();
+
+    if ($user && $user->role === 'USER') {
+        if (is_null($user->nik) || is_null($user->negara) || is_null($user->provinsi)) {
+        return redirect()->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors([
+                $this->username() => 'Your profile is incomplete. Please contact support to complete your profile before logging in.',
+            ]);
+    }}
+
+    return redirect()->back()
+        ->withInput($request->only($this->username(), 'remember'))
+        ->withErrors([
+            $this->username() => trans('auth.failed'),
+        ]);
+}
+
 }
