a293f485c6753ef7cd3a3187ef230989edf1bfd1
unknown <kenzhuyo@gmail.com>
2024-07-06 05:52:37 +0700

    feat/OTP&create proj done/reporting sisa finishing/matrix report blm

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -1,6 +1,8 @@
 <?php
 
 namespace App\Providers;
+use Illuminate\Support\Facades\Mail;
+use Swift_SmtpTransport;
 
 use Illuminate\Support\ServiceProvider;
 
@@ -23,6 +25,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Mail::getSwiftMailer()->getTransport()->setStreamOptions([
+            'ssl' => [
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+            ],
+        ]);
     }
 }
